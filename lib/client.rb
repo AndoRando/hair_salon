355df852aa1e3b568efa09b1bdf70286f081cf46
Andrew Brennwald <leopawnz@gmail.com>
2015-08-21 13:14:34 -0700

    Implement client method for adding clients to a stylist.

--- lib/client.rb
+++ lib/client.rb
@@ -1,9 +1,10 @@
 class Client
-  attr_reader(:name, :id)
+  attr_reader(:name, :id, :stylist_id)
 
   define_method(:initialize) do |attr|
     @name = attr.fetch(:name)
     @id = attr.fetch(:id)
+    @stylist_id = attr.fetch(:stylist_id)
   end
 
   define_method(:==) do |other_client|
@@ -13,16 +14,17 @@ class Client
   define_singleton_method(:all) do
     returned_clients = DB.exec("SELECT * FROM clients;")
     clients = []
-    returned_clients.each() do |stylist|
-      name = stylist.fetch("name")
-      id = stylist.fetch("id").to_i()
-      clients.push(Client.new({:name => name, :id => id}))
+    returned_clients.each() do |client|
+      name = client.fetch("name")
+      id = client.fetch("id").to_i()
+      stylist_id = client.fetch("stylist_id").to_i()
+      clients.push(Client.new({:name => name, :id => id, :stylist_id => stylist_id}))
     end
     clients
   end
 
   define_method(:save) do
-    save_me = DB.exec("INSERT INTO clients (name) VALUES ('#{@name}') RETURNING id;")
+    save_me = DB.exec("INSERT INTO clients (name, stylist_id) VALUES ('#{@name}', #{@stylist_id}) RETURNING id;")
     @id = save_me.first().fetch("id").to_i()
   end
 end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -3,16 +3,16 @@ require('spec_helper')
 describe(Client) do
   describe('#name') do
     it('returns the name of a client') do
-      customer = Client.new({:name => 'Maurice', :id => nil})
-      expect(customer.name()).to(eq('Maurice'))
+      maurice = Client.new({:name => 'Maurice', :id => nil, :stylist_id => 1})
+      expect(maurice.name()).to(eq('Maurice'))
     end
   end
 
   describe('#==') do
     it('is the same client if it has the same name') do
-      customer1 = Client.new({:name => 'Maurice', :id => nil})
-      customer2 = Client.new({:name => 'Maurice', :id => nil})
-      expect(customer1).to(eq(customer2))
+      maurice1 = Client.new({:name => 'Maurice', :id => nil, :stylist_id => 1})
+      maurice2 = Client.new({:name => 'Maurice', :id => nil, :stylist_id => 1})
+      expect(maurice1).to(eq(maurice2))
     end
   end
 
@@ -24,17 +24,24 @@ describe(Client) do
 
   describe('#save') do
     it('saves a client to the database') do
-      customer = Client.new({:name => 'Maurice', :id => nil})
-      customer.save()
-      expect(Client.all()).to(eq([customer]))
+      maurice = Client.new({:name => 'Maurice', :id => nil, :stylist_id => 1})
+      maurice.save()
+      expect(Client.all()).to(eq([maurice]))
     end
   end
 
   describe('#id') do
     it('sets client ID when saved') do
-      customer = Client.new({:name => 'Maurice', :id => nil})
-      customer.save()
-      expect(customer.id()).to(be_an_instance_of(Fixnum))
+      maurice = Client.new({:name => 'Maurice', :id => nil, :stylist_id => 1})
+      maurice.save()
+      expect(maurice.id()).to(be_an_instance_of(Fixnum))
+    end
+  end
+
+  describe('#stylist_id') do
+    it('returns the stylist of a specific client') do
+      maurice = Client.new({:name => 'Maurice', :id => nil, :stylist_id => 1})
+      expect(maurice.stylist_id()).to(eq(1))
     end
   end
 end
