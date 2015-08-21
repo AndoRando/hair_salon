1b8f353a496746a245fa7a0397211e22e0da70c5
Andrew Brennwald <leopawnz@gmail.com>
2015-08-21 13:31:05 -0700

    Initialize Sinatra environment.

---
+++ app.rb
@@ -0,0 +1,24 @@
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/stylist')
+require('./lib/client')
+also_reload('lib/**/*.rb')
+require('pg')
+
+DB = PG.connect({:dbname => "hair_salon"})
+
+
+get('/') do
+  erb(:index)
+end
+
+get('/stylists/new') do
+  erb(:stylist_new)
+end
+
+post('/stylists') do
+  name = params.fetch('name')
+  stylist = Stylist.new({:name => name, :id => nil})
+  stylist.save()
+  erb(:success)
+end
